ce201bd2bd3a2c0e6c5da44a7e5e938704a415ac
Sergejus <seriog@gmail.com>
2014-01-09 16:29:07 +0200

    updated nuget path

--- .nuget/publish.ps1
+++ .nuget/publish.ps1
@@ -7,18 +7,19 @@ Param (
 
 # get script variables
 $nugetApiKey = $parameters["NuGetApiKey-secure"]
+$semver = $projectVersion + $parameters["BuildLabel"]
 
 # update package version in nuspec file
 Write-Output "Updating version in nuspec file"
 $nuspecPath = "$srcFolder\TplDataflow.Extensions\TplDataflow.Extensions.nuspec"
 [xml]$xml = Get-Content $nuspecPath
-$xml.package.metadata.version = $projectVersion + "-alpha"
+$xml.package.metadata.version = $semver
 $xml.Save($nuspecPath)
 
 # build NuGet package
 Write-Output "Building NuGet package"
-.\NuGet.exe pack $nuspecPath
+.\.nuget\NuGet.exe pack $nuspecPath
 
 # publish NuGet package
 Write-Output "Publishing NuGet package"
-.\NuGet.exe push $srcFolder\Adform.TplDataflow.Extensions.$projectVersion.nupkg $nugetApiKey
+.\.nuget\NuGet.exe push $srcFolder\Adform.TplDataflow.Extensions.$semver.nupkg $nugetApiKey
